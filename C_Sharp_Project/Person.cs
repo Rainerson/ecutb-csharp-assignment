5219a539f657c2787328a765b6dcc8193f0a478a
Rainerson <raha1618@student.ju.se>
2023-01-24 13:12:00 +0100

    Add project files.

---
+++ C_Sharp_Project/Person.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace C_Sharp_Project
+{
+    internal interface IPerson
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+
+        string LastName { get; set; } 
+
+        string Email { get; set; } 
+
+        string PhoneNumber { get; set; } 
+
+        string Adress { get; set; }
+
+        string ShowBasicPerson => $"Namn: {FirstName} {LastName} E-post: {Email}";
+        string ShowDetailedPerson => $"Förnamn: {FirstName} \nEfternamn: {LastName} \nE-post: {Email} \nTelefon: {PhoneNumber} \n Adress: {Adress}";
+    }
+    internal class Person : IPerson
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Adress { get; set; } = null!;
+    }
+}
